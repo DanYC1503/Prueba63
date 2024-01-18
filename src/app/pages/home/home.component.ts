84349db822ebe2becc377482ce58de0615153b7f
DanYC1503 <danielyanzacaceres@gmail.com>
2024-01-18 09:44:33 -0500

    ahora no lo rompe :D

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DeudasServiceService } from 'src/app/services/deudas-service.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { DeudasServiceService } from 'src/app/services/deudas-service.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  cedula: string | undefined;
+  cedula?: string ;
   deudas: any[] = [];
 
   constructor(private deudasService: DeudasServiceService) {
@@ -17,15 +18,7 @@ export class HomeComponent {
   buscarCedula() {
     if (this.cedula) {
       this.deudasService.getDeudasPorCedula(this.cedula)
-        .subscribe(
-          data => {
-            this.deudas = data;
-          },
-          error => {
-            console.error('Error in buscarCedula:', error);
-            // Handle the error as needed (e.g., show a user-friendly message)
-          }
-        );
+        
     }
   }
 }
--- src/app/services/deudas-service.service.ts
+++ src/app/services/deudas-service.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeudasServiceService {
-  private baseUrl = 'http://localhost:18080/Prueba63/rs/deudas';
 
   constructor(private http: HttpClient) {}
 
-  getDeudasPorCedula(cedula: string): Observable<any> {
-    const url = `${this.baseUrl}/${cedula}`;
-    return this.http.get(url);
+  getDeudasPorCedula(cedula: any) {
+    return this.http.get<any>('http://localhost:18080/Prueba63/rs/deudas/' + cedula)
   
-}
+  }
   
 }
